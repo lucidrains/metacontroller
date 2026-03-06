working internal RL on BabyAI. Fixed: modality switching (rgb/symbolic/rgb with resnet, unused); video logging in the internal RL phase; fixed double weighting for KL and ratio loss; fixed modality switching in trajectory gathering script

train_enwik8.py
@@ -32,6 +32,7 @@ from torch.optim import AdamW
 from torch.utils.data import DataLoader, Dataset
 
 from accelerate import Accelerator
+from accelerate.utils import DistributedDataParallelKwargs
 from einops import rearrange
 
 from metacontroller import MetaController, Transformer, binary_entropy
@@ -246,7 +247,8 @@ def train(
 ):
     # accelerator
 
-    accelerator = Accelerator(cpu = cpu)
+    ddp_kwargs = DistributedDataParallelKwargs(find_unused_parameters=True)
+    accelerator = Accelerator(cpu = cpu, kwargs_handlers=[ddp_kwargs])
 
     if (grpo_phase or evo_phase) and accelerator.is_main_process:
         import wandb